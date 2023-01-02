ec8fc4df8162de1d02c3477af51d478aa2f24d82
makinera <121771947+makinera@users.noreply.github.com>
2023-01-02 20:49:52 -0300

    Update Teste_FKMakino.py
    Adicionada as soluções da matriz

--- Teste_FKMakino.py
+++ Teste_FKMakino.py
@@ -64,3 +64,79 @@ df_pergunta7\
   .option('header', 'true')\
   .option('delimiter', ';')\
   .save('S3_path') #Pergunta 8
+  
+  
+#Matriz - Solução 1
+ 
+import numpy as np
+
+# cria uma matriz com numeros inteiros randomicos de 0 a 10
+mA = np.random.randint(10, size=(4,4))
+
+mB = np.random.randint(10, size=(4,4))
+
+#multiplica a matriz A com a B
+mProduto = np.matmul(mA, mB)
+
+print('Matriz A:'+chr(10))
+print(mA)
+
+print(chr(10)+'Matriz B:'+chr(10))
+print(mB)
+
+print(chr(10)+'Matriz Producto, resultado da multiplicacao da A e B:'+chr(10))
+
+print(mProduto)
+
+#Matriz - Solução 2
+
+import random
+# funcao gera uma matriz quadrada
+def matriz_quadrada():
+    l = []
+    for rr in range(0,4):
+        #a.append()
+        l_row = [random.randint(0,10) for x in range(0,4)]
+        l.append(l_row)
+    
+    return l
+
+
+mA = matriz_quadrada() # criando a matriz A
+mB = matriz_quadrada() # criando a matriz B
+
+#mA = [[1,2,3,4], [5,6,7,8], [1,2,3,4], [5,6,7,8]] hardcode valores do exemplo na questão 
+
+#mB = [[1,2,3,4], [5,6,7,8], [1,2,3,4], [5,6,7,8]] hardcode valores do exemplo na questão 
+print('Matriz A:')
+for rr in mA:
+    print(rr)
+
+print(chr(10)+'Matriz B:')
+for rr in mB:
+    print(rr)
+
+
+
+
+m_Produdo = []
+l_total   = []
+for index,l_mA in enumerate(mA):      # loop com o index e valor da matriz A
+    l_total = []
+    for rr in range(0,len(l_mA)):     # loop para varrer a linha da matriz A o numero de vezes do tamanho da matriz
+        l_mB = [x[rr] for x in mB]    # loop para varrer a coluna da matriz B
+        total = 0
+        
+        for lA, lB in zip(l_mA, l_mB): # loop para multiplicar a linha da matriz A com a coluna da matriz B
+            total = total+lA*lB       
+        
+        l_total.append(total)
+    m_Produdo.append(l_total)
+
+
+
+
+# print do resultado:
+print(chr(10)+"resultado da Matriz produto:"+chr(10))
+for rr in m_Produdo:
+    print(rr)
